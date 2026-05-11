f455a2879bdac2f1e83ce1a4fb4ed3bd1ab8eeb6
BhargavaLBharadwaja <147256963+BhargavaLBharadwaja@users.noreply.github.com>
2026-05-11 13:41:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-# from flask import Flask
-# app=Flask(__name__)
-# @app.route("/")
-# def home():
-#     return "Hello ! docker running successfully"
-# if __name__="__main__":
-#     app.run(host="0.0.0.0",port=5002)
 from flask import Flask
-
-app = Flask(__name__)
-
+app=Flask(__name__)
 @app.route('/')
 def home():
-    return "Docker Jenkins App Running Successfully!"
+    return "Hello ! docker running successfully"
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=5002)
+# from flask import Flask
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "Docker Jenkins App Running Successfully!"
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5002)
